80aea3a66f5103ac44cfe81a83825012e1ccf38c
Kai Takahashi <www.carrotsoft@gmail.com>
2016-11-28 21:26:46 +0900

    Cleanup files.

--- Gamboh.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Enemy.h"
-typedef struct{
-	
-} Gamboh_Field;
-
-Enemy* Gamboh_New();
-void Gamboh_Dispose(Enemy*);
--- a.c
+++
@@ -1,6 +0,0 @@
-int main()
-{
-	int* a = 0x7ffff5;
-	*a = 5;
-	return 0;
-}
